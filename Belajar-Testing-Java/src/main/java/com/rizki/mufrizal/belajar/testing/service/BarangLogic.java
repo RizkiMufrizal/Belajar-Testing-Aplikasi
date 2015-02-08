ce1f77405063d24ee548964e2c054bcc89586cf5
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-08 11:11:25 +0700

    Update Logic Barang

--- Belajar-Testing-Java/src/main/java/com/rizki/mufrizal/belajar/testing/service/BarangLogic.java
+++ Belajar-Testing-Java/src/main/java/com/rizki/mufrizal/belajar/testing/service/BarangLogic.java
@@ -2,10 +2,6 @@ package com.rizki.mufrizal.belajar.testing.service;
 
 import com.rizki.mufrizal.belajar.testing.domain.Barang;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class BarangLogic {
     
     public String kelengkapanBarang(Barang barang){
@@ -13,12 +9,7 @@ public class BarangLogic {
             return "barang tidak ditemukan";
         }
         
-        return "Spesifikasi Barang : " + barang.getIdBarang() + " : " + barang.getNamaBarang() + " : " + barang.getJenisBarang() + " : " + barang.getTanggalKadaluarsa();
-    }
-    
-    public Date checkingDateFormat(Barang barang) throws ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        return format.parse(barang.getTanggalKadaluarsa().toString());
+        return "Spesifikasi Barang : " + barang.getIdBarang() + " : " + barang.getNamaBarang() + " : " + barang.getJenisBarang() + " : " + barang.getTanggalKadaluarsa() + " : " + barang.getHargaBarang();
     }
     
 }
