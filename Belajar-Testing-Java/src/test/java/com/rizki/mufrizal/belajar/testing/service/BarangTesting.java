5f55b4aa8d8d82a42494b496a9a4ea0c10258875
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-08 11:12:53 +0700

    Add Barang Testing Main

---
+++ Belajar-Testing-Java/src/test/java/com/rizki/mufrizal/belajar/testing/service/BarangTesting.java
@@ -0,0 +1,13 @@
+package com.rizki.mufrizal.belajar.testing.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BarangLogicTest.class,
+        BarangLogicJumlahTest.class
+    }
+)
+public class BarangTesting {
+}
