efc3492f94716f4b98e6f4f20cde8c196eb9c48d
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-08 11:11:46 +0700

    Add Logic Jumlah Barang

---
+++ Belajar-Testing-Java/src/main/java/com/rizki/mufrizal/belajar/testing/service/BarangLogicJumlah.java
@@ -0,0 +1,11 @@
+package com.rizki.mufrizal.belajar.testing.service;
+
+import com.rizki.mufrizal.belajar.testing.domain.Barang;
+
+public class BarangLogicJumlah {
+    
+    public Integer totalBarang(Barang barang, Integer jumlahBarang){
+        return barang.getHargaBarang() * jumlahBarang;
+    }
+    
+}
