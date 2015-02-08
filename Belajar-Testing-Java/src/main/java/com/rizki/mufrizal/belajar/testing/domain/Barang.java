c99bf3e7a7c85d71ab7a0ece3d9c4131675a8f5a
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-08 10:01:06 +0700

    Add Domain Barang

---
+++ Belajar-Testing-Java/src/main/java/com/rizki/mufrizal/belajar/testing/domain/Barang.java
@@ -0,0 +1,44 @@
+package com.rizki.mufrizal.belajar.testing.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Barang implements Serializable{
+    
+    private Integer idBarang;
+    private String namaBarang;
+    private JenisBarang jenisBarang;
+    private Date tanggalKadaluarsa;
+
+    public Integer getIdBarang() {
+        return idBarang;
+    }
+
+    public void setIdBarang(Integer idBarang) {
+        this.idBarang = idBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public JenisBarang getJenisBarang() {
+        return jenisBarang;
+    }
+
+    public void setJenisBarang(JenisBarang jenisBarang) {
+        this.jenisBarang = jenisBarang;
+    }
+
+    public Date getTanggalKadaluarsa() {
+        return tanggalKadaluarsa;
+    }
+
+    public void setTanggalKadaluarsa(Date tanggalKadaluarsa) {
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+}
