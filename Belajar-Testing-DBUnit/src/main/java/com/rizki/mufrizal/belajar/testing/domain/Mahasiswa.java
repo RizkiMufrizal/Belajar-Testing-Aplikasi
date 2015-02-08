578c43d346290bb3c523cafc136debf6ddefafb5
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-08 15:14:25 +0700

    Add Domain Mahasiswa

---
+++ Belajar-Testing-DBUnit/src/main/java/com/rizki/mufrizal/belajar/testing/domain/Mahasiswa.java
@@ -0,0 +1,53 @@
+package com.rizki.mufrizal.belajar.testing.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Mahasiswa implements Serializable{
+
+    private String npm;
+    private String nama;
+    private String kelas;
+    private String jurusan;
+    private Date tanggalLahir;
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+}
