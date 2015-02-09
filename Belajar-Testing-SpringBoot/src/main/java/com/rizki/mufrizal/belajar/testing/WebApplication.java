765bda3487b256e09f7e244267c189c2c26b1e3f
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-09 09:20:31 +0700

    Add Main Class Spring Boot

---
+++ Belajar-Testing-SpringBoot/src/main/java/com/rizki/mufrizal/belajar/testing/WebApplication.java
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.belajar.testing;
+
+import com.rizki.mufrizal.belajar.testing.config.WebConfig;
+import org.springframework.boot.SpringApplication;
+
+public class WebApplication {
+    
+    public static void main(String[]args){
+        SpringApplication.run(WebConfig.class);
+    }
+    
+}
