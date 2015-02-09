52ce56594573e2c6ab3d826cfcfb2f88dcf73f10
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-09 10:30:56 +0700

    Add Find By Tanggal

--- Belajar-Testing-SpringBoot/src/main/java/com/rizki/mufrizal/belajar/testing/service/TiketService.java
+++ Belajar-Testing-SpringBoot/src/main/java/com/rizki/mufrizal/belajar/testing/service/TiketService.java
@@ -2,6 +2,7 @@ package com.rizki.mufrizal.belajar.testing.service;
 
 import com.rizki.mufrizal.belajar.testing.domain.Tiket;
 
+import java.util.Date;
 import java.util.List;
 
 public interface TiketService {
@@ -9,5 +10,6 @@ public interface TiketService {
     public void update(Tiket tiket);
     public void delete(Tiket tiket);
     public Tiket getTiket(String idTiket);
+    public Tiket getTiketBytanggalKeberangkatan(Date date);
     public List<Tiket> getTiketList();
 }
