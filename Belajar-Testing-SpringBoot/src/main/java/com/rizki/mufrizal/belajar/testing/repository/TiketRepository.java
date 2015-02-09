f0e32b41d3a791ca586d78f2fdebb0a63c46b1c4
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-09 10:29:59 +0700

    Add Find Tanggal

--- Belajar-Testing-SpringBoot/src/main/java/com/rizki/mufrizal/belajar/testing/repository/TiketRepository.java
+++ Belajar-Testing-SpringBoot/src/main/java/com/rizki/mufrizal/belajar/testing/repository/TiketRepository.java
@@ -2,8 +2,13 @@ package com.rizki.mufrizal.belajar.testing.repository;
 
 import com.rizki.mufrizal.belajar.testing.domain.Tiket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository
 public interface TiketRepository extends JpaRepository<Tiket, String>{
+    public Tiket findBytanggalKeberangkatan(Date date);
 }
