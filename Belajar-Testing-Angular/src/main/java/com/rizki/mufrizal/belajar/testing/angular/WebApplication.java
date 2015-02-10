8425dcc7ce1e6eab3a4e6f13af9b95ffbe12e131
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-10 09:25:19 +0700

    Update Main Class

--- Belajar-Testing-Angular/src/main/java/com/rizki/mufrizal/belajar/testing/angular/WebApplication.java
+++ Belajar-Testing-Angular/src/main/java/com/rizki/mufrizal/belajar/testing/angular/WebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 public class WebApplication extends WebMvcConfigurerAdapter{
-    
+
     public static void main(String[]args){
         SpringApplication.run(WebApplication.class);
     }
